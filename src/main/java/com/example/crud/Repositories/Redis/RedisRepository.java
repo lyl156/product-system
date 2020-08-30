e294116cec189f68d21c8331a7380f6981ba8539
lyllyl156 <r08922133@csie.ntu.edu.tw>
2020-08-30 16:26:30 +0800

    add Redis zset ops for rank service

---
+++ src/main/java/com/example/crud/Repositories/Redis/RedisRepository.java
@@ -0,0 +1,26 @@
+package com.example.crud.Repositories.Redis;
+
+import org.springframework.data.redis.core.ZSetOperations;
+
+import java.util.Set;
+
+public interface RedisRepository {
+    //z_set operation
+    //https://docs.spring.io/spring-data/redis/docs/current/api/org/springframework/data/redis/core/ZSetOperations.html#remove-K-java.lang.Object...-
+    void add(String key, String value, double score);
+
+    void remove(String key, String value);
+
+    Long size(String key);
+
+    Double getElementScore(String key, String value);
+
+    //higher score , higher rank
+    Long getReverseRank(String key, String value);
+
+    Set<String> getTopElement(String key, int start, int end);
+
+    Set<ZSetOperations.TypedTuple<String>> getTopElementWithScore(String key, int start, int end);
+
+
+}
---
+++ src/main/java/com/example/crud/Repositories/Redis/RedisRepositoryImpl.java
@@ -0,0 +1,52 @@
+package com.example.crud.Repositories.Redis;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public class RedisRepositoryImpl implements RedisRepository {
+    private RedisTemplate redisTemplate;
+
+    public RedisRepositoryImpl(RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public void add(String key, String value, double score) {
+        redisTemplate.opsForZSet().add(key, value, score);
+    }
+
+    @Override
+    public void remove(String key, String value) {
+        //return number of removed elements
+        redisTemplate.opsForZSet().remove(key, value);
+    }
+
+    @Override
+    public Long size(String key) {
+        return redisTemplate.opsForZSet().zCard(key);
+    }
+
+    @Override
+    public Double getElementScore(String key, String value) {
+        return redisTemplate.opsForZSet().score(key, value);
+    }
+
+    @Override
+    public Long getReverseRank(String key, String value) {
+        return redisTemplate.opsForZSet().reverseRank(key, value);
+    }
+
+    @Override
+    public Set<String> getTopElement(String key, int start, int end) {
+        return redisTemplate.opsForZSet().reverseRange(key, start, end);
+    }
+
+    @Override
+    public Set<ZSetOperations.TypedTuple<String>> getTopElementWithScore(String key, int start, int end) {
+        return redisTemplate.opsForZSet().reverseRangeWithScores(key, start, end);
+    }
+}
